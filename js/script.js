aad54680fdf266a0b925f14ddeb000e33b84b2b9
mcosta1206 <marciodacostaferreirajunior@gmail.com>
2023-11-06 21:38:19 -0300

    Atualização 2.0 Pokedex

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
 
 const fetchPokemon = async (pokemon) => {
-  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
   const data = await APIResponse.json();
   return data;
 }
@@ -13,7 +17,14 @@ const renderPokemon = async (pokemon) => {
 
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
+    ['animated']['front_default'];
+
+    input.value = '';
 
 }
 
-renderPokemon('pichu')
+form.addEventListener('submit',(event) => {
+  event.preventDefault();  
+  renderPokemon(input.value.toLowerCase());
+});
